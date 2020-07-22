7e453bbaa418ff6a50e6fd7f3146abf295d87a80
nguyendotnet <ndpn.dev@gmail.com>
2020-07-23 02:17:44 +0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// text animation delay
+function scrollText() {
+    var introText = document.querySelector('.text-animation');
+    var introPositon = introText.getBoundingClientRect().top;
+    var scrollPosition = window.innerHeight;
+
+
+    if (introPositon < scrollPosition) {
+        introText.classList.add('.text-animation-late');
+    }
+}
+//call function scrollText
+window.addEventListener('scroll', scrollText);
+
+
+
+
+
 //swipperCrousel
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
